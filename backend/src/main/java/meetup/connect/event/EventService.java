7dda6e819f6f191274e044ee7f76995c18c72cc4
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2024-03-14 12:43:30 +0100

    code format

--- backend/src/main/java/meetup/connect/event/EventService.java
+++ backend/src/main/java/meetup/connect/event/EventService.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class EventService {
 
@@ -25,9 +24,7 @@ public class EventService {
   public PageResponse<EventDto> getPage(Integer page, Integer size) {
     PageRequest pageRequest = PageRequest.of(page, size);
 
-    Page<EventDto> entities = eventRepository
-            .findAll(pageRequest)
-            .map(EventDto::fromEntity);
+    Page<EventDto> entities = eventRepository.findAll(pageRequest).map(EventDto::fromEntity);
 
     return new PageResponse<>(entities);
   }
